e4f13ea521561ba370bba6a3e06a3d778980bdb8
migros1 <migroscasa@gmail.com>
2024-12-03 13:00:48 +0300

    Update server.js

--- lmagpt/server.js
+++ lmagpt/server.js
@@ -3,7 +3,7 @@ const bodyParser = require('body-parser');
 const axios = require('axios');
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = 3000;
 
 app.use(bodyParser.json());
 app.use(express.static('public'));
